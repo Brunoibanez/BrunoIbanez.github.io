0df75b5fa53190eba74b929c2f49c72bed00969d
Brunoibanez <124073585+Brunoibanez@users.noreply.github.com>
2023-01-31 14:21:55 -0300

    Initial commit

---
+++ funcion.js
@@ -0,0 +1,3 @@
+document.getElementById("boton").onclick= function (){
+    console.log("Otros conocimientos")
+ document.getElementById ("demo").innerHTML ="Analista Financiero Certificado, Institucion Salvador, 2022" }
